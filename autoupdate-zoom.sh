e54f8de56947e5a69ff956765ee6ccbcbd5a6cb5
Patrick Zeller <patrick.zeller@exxcellent.de>
2021-03-29 15:42:43 +0200

    refactor(autoupdate-zoom.sh): disable parameter expansion in here-documents
    Use quoted form of delimiter to disable parameter expansion, command substitution, and arithmetic expansion in file content string.
    Improves readability of created files in the shell script.

--- autoupdate-zoom.sh
+++ autoupdate-zoom.sh
@@ -30,18 +30,18 @@ subcommand=$1
 case "$subcommand" in
 install)
   mkdir /opt/zoom-updater
-  cat <<EOF >/opt/zoom-updater/zoom-update.sh
+  cat <<'EOF' >/opt/zoom-updater/zoom-update.sh
 #!/bin/bash
 export LANG=en
-ZOOM_VERSION_AVAILABLE=\$(curl -s 'https://zoom.us/support/download' --header 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' | grep "class=\"linux-ver-text\"" | sed -e 's/.*Version \(.*\)<.*/\1/')
-echo zoom version available for download: \$ZOOM_VERSION_AVAILABLE
-ZOOM_VERSION_AVAILABLE_MAJOR=\$(echo \$ZOOM_VERSION_AVAILABLE | sed -e 's/\([^\.]\+\.[^\.]\+\).*/\1/')
-echo "major" zoom version available for download: \$ZOOM_VERSION_AVAILABLE_MAJOR
-ZOOM_VERSION_AVAILABLE_MINOR=\$(echo \$ZOOM_VERSION_AVAILABLE | sed -e 's/[^\(]\+(\(.*\)).*/\1/')
-echo "minor" zoom version available for download: \$ZOOM_VERSION_AVAILABLE_MINOR
-ZOOM_VERSION_INSTALLED=\$(apt-cache policy zoom | grep "Installed:" | sed -e 's/.*Installed: \(.*\)/\1/')
-echo zoom version installed: \$ZOOM_VERSION_INSTALLED
-if [[ "\$ZOOM_VERSION_INSTALLED" != *"\$ZOOM_VERSION_AVAILABLE_MINOR"* ]] || [[ "\$ZOOM_VERSION_INSTALLED" != *"\$ZOOM_VERSION_AVAILABLE_MAJOR"* ]]; then
+ZOOM_VERSION_AVAILABLE=$(curl -s 'https://zoom.us/support/download' --header 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' | grep "class=\"linux-ver-text\"" | sed -e 's/.*Version \(.*\)<.*/\1/')
+echo zoom version available for download: $ZOOM_VERSION_AVAILABLE
+ZOOM_VERSION_AVAILABLE_MAJOR=$(echo $ZOOM_VERSION_AVAILABLE | sed -e 's/\([^\.]\+\.[^\.]\+\).*/\1/')
+echo "major" zoom version available for download: $ZOOM_VERSION_AVAILABLE_MAJOR
+ZOOM_VERSION_AVAILABLE_MINOR=$(echo $ZOOM_VERSION_AVAILABLE | sed -e 's/[^\(]\+(\(.*\)).*/\1/')
+echo "minor" zoom version available for download: $ZOOM_VERSION_AVAILABLE_MINOR
+ZOOM_VERSION_INSTALLED=$(apt-cache policy zoom | grep "Installed:" | sed -e 's/.*Installed: \(.*\)/\1/')
+echo zoom version installed: $ZOOM_VERSION_INSTALLED
+if [[ "$ZOOM_VERSION_INSTALLED" != *"$ZOOM_VERSION_AVAILABLE_MINOR"* ]] || [[ "$ZOOM_VERSION_INSTALLED" != *"$ZOOM_VERSION_AVAILABLE_MAJOR"* ]]; then
    echo downloading new version...
    wget --quiet https://zoom.us/client/latest/zoom_amd64.deb -P /tmp
    export DEBIAN_FRONTEND=noninteractive
@@ -53,7 +53,7 @@ fi
 EOF
   chmod +x /opt/zoom-updater/zoom-update.sh
 
-  cat <<EOF >/etc/systemd/system/zoom-update.timer
+  cat <<'EOF' >/etc/systemd/system/zoom-update.timer
 [Unit]
 Description=Update zoom daily
 
@@ -65,7 +65,7 @@ Persistent=true
 WantedBy=timers.target
 EOF
 
-  cat <<EOF >/etc/systemd/system/zoom-update.service
+  cat <<'EOF' >/etc/systemd/system/zoom-update.service
 [Unit]
 Description=zoom update service
 After=network.target
